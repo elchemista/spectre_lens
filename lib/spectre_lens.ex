655b392bd0869c42b84388a33d5ffb440a10d851
Yuriy Zhar <darkyfelis@gmail.com>
2026-04-17 23:06:16 +0200

    init

---
+++ lib/spectre_lens.ex
@@ -0,0 +1,18 @@
+defmodule SpectreLens do
+  @moduledoc """
+  Documentation for `SpectreLens`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpectreLens.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spectre_lens_test.exs
@@ -0,0 +1,8 @@
+defmodule SpectreLensTest do
+  use ExUnit.Case
+  doctest SpectreLens
+
+  test "greets the world" do
+    assert SpectreLens.hello() == :world
+  end
+end
